a3dcacb2690cb91d6c1957e79784c7176b31aded
Lucas Amorim <lucas.de.amorimarq@gmail.com>
2024-09-23 00:05:22 -0300

    Testes automaticos

--- agenda_web/agenda/tests.py
+++ agenda_web/agenda/tests.py
@@ -11,5 +11,7 @@ class AgentaTest(TestCase):
         Agenda.objects.create(nome='test2', data_ini=datetime.now(), data_fim=datetime.now(), peso=1)
 
     def test_agenda_criada(self):
-        agenda = Agenda.objects.get(pk=2)
-        self.assertEqual(agenda.nome,'test')
+        agenda1 = Agenda.objects.get(pk=1)
+        agenda2 = Agenda.objects.get(pk=2)
+        self.assertEqual(agenda1.nome,'test')
+        self.assertEqual(agenda2.nome,'test2')
